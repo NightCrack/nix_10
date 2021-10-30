e7a5dcc3677b7fca5cbbbcd999067383767a2159
NightCrack <NightCrack13@gmail.com>
2021-10-30 13:19:55 +0300

    Module 1
    Level 1:
    
    (1) Count unique numbers
    (2) Check horse's move
    (3) Calculate an area of a triangular
    
    Level 2:
    
    (1) String validation
    (2) Generate and seize random tree
    
    Level 3:
    
    Game of Life

---
+++ module_1/src/main/java/ua/com/alevel/level1/entity/Dot.java
@@ -0,0 +1,8 @@
+package ua.com.alevel.level1.entity;
+
+public class Dot extends BaseEntity{
+    @Override
+    public String toString() {
+        return "Dot(" + super.getCoordinateX() + super.getCoordinateY() + ") ";
+    }
+}
