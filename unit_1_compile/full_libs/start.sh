7658efefa3880a241c53f98e5546953b983911ef
NightCrack <NightCrack13@gmail.com>
2021-11-02 22:12:41 +0200

    Removed class projects from remote

--- unit_1_compile/full_libs/start.sh
+++
@@ -1,2 +0,0 @@
-javac -d build/classes/ -sourcepath src/ -cp libs/commons-lang3-3.12.0.jar src/ua/com/alevel/Main.java
-java -cp build/classes/:libs/commons-lang3-3.12.0.jar ua.com.alevel.Main
--- unit_1_compile/simple/simple_package/ua/com/alevel/Main.java
+++
@@ -1,9 +0,0 @@
-package ua.com.alevel;
-
-class Main {
-    public static void main(String[] args) {
-        Main main = new Main();
-        System.out.println(main.getClass().getName());
-        System.out.println(main.getClass().getSimpleName());
-    }
-}
